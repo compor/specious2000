ca93d84798d0dd31ce53ca87cef1b5d401ed9238
Chris Vassiladiotis <cvassiladiotis@gmail.com>
2020-02-25 11:44:17 +0000

    Add invocations

---
+++ utils/invocations/ref/175.vpr.ref.cmd
@@ -0,0 +1,2 @@
+net.in arch.in place.out dum.out -nodisp -place_only -init_t 5 -exit_t 0.005 -alpha_t 0.9412 -inner_num 2 1> place_log.out 2> place_log.err
+net.in arch.in place.in route.out -nodisp -route_only -route_chan_width 15 -pres_fac_mult 2 -acc_fac 1 -first_iter_pres_fac 4 -initial_pres_fac 8 1> route_log.out 2> route_log.err
